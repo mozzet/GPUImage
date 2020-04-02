c79dc67a90c8beaf1ec2cc887df4d58d4c5977fe
Jo Sesang <ss.cho@mozzet.com>
2020-04-02 17:55:33 +0900

    카르타고 지원되도록 변경

---
+++ GPUImage/GPUImage.h
@@ -0,0 +1,19 @@
+//
+//  GPUImage.h
+//  GPUImage
+//
+//  Created by MOZZET PC on 2020/04/02.
+//  Copyright © 2020 Noondate. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GPUImage.
+FOUNDATION_EXPORT double GPUImageVersionNumber;
+
+//! Project version string for GPUImage.
+FOUNDATION_EXPORT const unsigned char GPUImageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GPUImage/PublicHeader.h>
+
+
